08250dd3a56f970edef4135f01163f4567b82bb6
NgTrHau <92238714+NgTrHau@users.noreply.github.com>
2022-05-06 20:10:02 +0700

    update

--- partials/mysqli_connect.php
+++ partials/mysqli_connect.php
@@ -5,7 +5,7 @@ mysqli_report(MYSQLI_REPORT_OFF);
 
 // Ký tự @ dùng để tắt các cảnh bảo (warning) sinh ra bởi câu lệnh
 
-$dbc = @mysqli_connect('localhost', 'root', '', 'ct275_web');
+$dbc = @mysqli_connect('localhost', 'root', '', 'ct46702');
 
 if (!$dbc) {
 	echo '<p class="error">Không thể kết nối đến CSDL vì:<br>' .
